9eead9ec8fee4d1aec7d9f9c3043edd58331c627
jomezc <73670380+jomezc@users.noreply.github.com>
2022-06-16 12:16:54 +0200

    Commit con la definición de la prueba unitaria de la tarea 2.2 Envío por correo de informes: la prueba falla al no estar implementada todavía la funcionalidad, siguiendo TDD

--- test/Test_WebScrapetUniversal.py
+++ test/Test_WebScrapetUniversal.py
@@ -8,6 +8,77 @@ Grado en Ingeniería Informática - Web Scraping universal con entrada - UNIR
 import unittest, os, sys
 from lib.WebScraperSelenium import WebScraperSelenium
 
+import imaplib
+import os
+import email
+
+def descargaMail(usuario, contraseña, host, puerto, ruta):
+    """
+    Versión modificada basada en:  (Doshi, 2018). To read emails and download attachments in Python
+    Obtenido de stackoverflow: https://medium.com/@sdoshi579/to-read-emails-and-download-attachments-in-python-6d7d6b60269
+    Función que se descarga los adjuntos del correo nuevo y devuelve los datos del email
+    """
+    correo_descargado = [] # lista para introducir los archivos descargados
+
+    #  Accede vía IMAP4 al host y puerto especificado mediante los datos de acceso
+    mail = imaplib.IMAP4_SSL(host, puerto)
+    mail.login(usuario, contraseña)
+
+    # seleccionas la carpeta o buzón donde quieres descargar el correo
+    mail.select()
+
+    # .search busca desde el correo
+    # podríamos seleccionar filtros como desde, hacia o el asunto del correo
+    # None es un conjunto de caracteres y ALL devuelve todos los mensajes sin ningún filtro.
+    tipo, datos = mail.search(None, 'ALL')
+    mail_ids = datos[0]
+    # Esta función devuelve el tipo que es si la solicitud estaba bien o no.
+    # Los datos son identificadores de todos los correos electrónicos.
+    id_list = mail_ids.split()
+
+    for num in datos[0].split():
+        # fetch obtiene el correo para un identificador determinado
+        # 'RFC822' es un protocolo de acceso a mensajes de Internet.
+        typ, datos = mail.fetch(num, '(RFC822)')
+        raw_email = datos[0][1]
+
+        # Los datos de fetch están codificados en binario,
+        # por lo que necesitamos decodificar en el conjunto de caracteres UTF-8.
+        raw_email_string = raw_email.decode('utf-8')
+        # Ahora, pasamos esa cadena decodificada a email.message_from_string
+        # que acepta una cadena y la convierte en formato de diccionario
+        email_message = email.message_from_string(raw_email_string)
+
+        # Descarga de adjuntos
+        for parte in email_message.walk():
+
+            if parte.get_content_maintype() == 'multipart':
+                continue
+            if parte.get('Content-Disposition') is None:
+                continue
+            fileName = parte.get_filename()
+            if bool(fileName):
+                filePath = os.path.join(ruta, fileName)
+                if not os.path.isfile(filePath):
+                    fp = open(filePath, 'wb')
+                    fp.write(parte.get_payload(decode=True))
+                    fp.close()
+                subject = str(email_message).split("Subject: ", 1)[1].split("\nTo:", 1)[0]
+                correo_descargado.append(f'Hemos descargado "{fileName}" del mail con asunto "{subject}".')
+
+    for parte_Respuesta in datos:
+        if isinstance(parte_Respuesta, tuple):
+            msg = email.message_from_string(parte_Respuesta[1].decode('utf-8'))
+            email_subject = msg['subject']
+            email_from = msg['from']
+            correo_descargado.append('From : ' + email_from + '\n')
+            correo_descargado.append('Subject : ' + email_subject + '\n')
+            correo_descargado.append(msg.get_payload(decode=True)) # cuerpo
+    mail.close()
+    del mail
+    return correo_descargado
+
+
 def ruta_relativa(ruta_relativa):
     """
     Versión modificada basada en:  Rahimi, M. (21 de 07 de 2021). Relative path setting fail via pyinstaller.
@@ -47,6 +118,17 @@ class TestWebScraperSelenium(unittest.TestCase):
             'Atención al ciudadano\n',
             'XPATH.DOUBLE_CLICK: /html/body/div[4]/div/div[1]/div/ul/li[2]/a'}
         self.datos_descarga = ['A29313-29424.pdf']
+        self.adjuntos_mail = ['A29313-29424.pdf']
+        self.datos_mail = {
+            'A29313-29424.pdf',
+            'Extración_web',
+            'fulgenciovalleverde@gmail.com'}
+        self.usuario = 'fulgenciovalleverde@gmail.com'
+        self.contraseña = 'gfmxjfwesjygolge'
+        self.host = 'imap.gmail.com'
+        self.puerto = 993
+        self.ruta = '/Users/jesusgomezcanovas/Dropbox/guapo/programar/python/WSU/test/DescargaEmail/'
+
 
     def abrirFichero(self):
         try:
@@ -72,6 +154,7 @@ class TestWebScraperSelenium(unittest.TestCase):
         for dato in self.datos_navegacion:
             error = f'La página {dato} debía haber sido visitada y no está registrada'
             self.assertEqual(dato in self.resultado, True, error)  # Compara el resultado con lo esperado (sea igual)
+        del self.WebScraperSelenium
 
     def testFichero(self):
         """
@@ -106,6 +189,30 @@ class TestWebScraperSelenium(unittest.TestCase):
             resultado = fichero in os.listdir(ruta_relativa('archivos/'))
             self.assertEqual(resultado, True, error)
 
+
+    def testEmail(self):
+        # ACT, invocamos al método de descarga de mail
+        correo_descargado = descargaMail(self.usuario, self.contraseña,
+                                         self.host, self.puerto, self.ruta)
+        # ASSER, validamos los resultados
+
+        # Ficheros en directorio
+        for fichero in self.adjuntos_mail:
+            error = f'fichero {fichero} no encontrado'
+            resultado = fichero in os.listdir(ruta_relativa('test/DescargaEmail/'))
+            self.assertEqual(resultado, True, error)
+
+        # compresión de lista para comprobar si el esperado (datos_fichero)
+        # está en el fichero cargado (contenido_fichero) (Delfstack, 2021)
+        resultado = {dato for dato in self.datos_mail if any(dato in linea for linea in correo_descargado)}
+        error = f"""
+                No se han encontrado todos los pasos en el fichero, el contenido es
+                {correo_descargado}
+                Se ha encontrado: {resultado}
+                Esperaba: {self.datos_fichero}
+                * Son conjuntos, por lo que no existe el orden ni repeticiones, lo importante es que estén todos
+        """
+        self.assertEqual(resultado, self.datos_mail, error)
 
 #   MAIN
 if __name__ == '__main__':
